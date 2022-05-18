aa805d59fa9f5524770316cf33ac039e4f4a39cb
Maia Davis <maia@Maias-Air.fios-router.home>
2022-05-17 21:43:01 -0400

    added element for not on display and recent search buttons via local storage

--- assets/script.js
+++ assets/script.js
@@ -12,18 +12,18 @@ var thumbnailEl = document.getElementById('thumbnail')
 var artworkInfo = document.getElementById('artworkInfo');
 var carousel = document.getElementById('artworkCarousel');
 
-let index = 0;
-
-var listOfArtworkSearches = document.getElementById("listArtworkSearched");
+var recentSearches = document.getElementById("localStorage");
 var containerElement = document.getElementById("currentSearch");
 
-function getApi(valueSearched) {
+
+function getApi() {
     //search Art Insitute Database
     var requestUrl = 'https://api.artic.edu/api/v1/artworks/search?q=' + inputValue.value + "&fields=id,title,image_id,artist_titles,is_on_view";
     console.log(requestUrl);
 
-    searchArray = JSON.parse(localStorage.getItem("searchedValue")) || [inputValue];
-    localStorage.setItem("searchedValue", JSON.stringify(searchArray));
+    var search = inputValue.value;
+    localStorage.setItem("recentSearch", search);
+
 
     //search Wikipedia Database
     //var WikipediaUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=" + inputValue.value;
@@ -37,25 +37,31 @@ function getApi(valueSearched) {
 
         .then(function (data) {
             console.log(data);
+            carousel.innerHTML = '';
 
-            //loop through titles for up to 6 artworks
+            //loop through data for up to 6 artworks
 
             for (var i = 0; i < 6; i++) {
 
+            //create title element
                 var titleParagraph = document.createElement('p');
                 console.log(titleParagraph);
 
+            //create artist name element
                 var artistParagraph = document.createElement('p');
+
+            //create element showing whether piece is on view
                 var onView = document.createElement('p');
 
+            //populate elements with relevant data
                 titleParagraph.textContent = "Title of Piece: " + data['data'][i]['title'];
                 artistParagraph.textContent = "Artist: " + data['data'][i]['artist_titles'][0];
 
                 if (data['data'][i]['is_on_view'] === true) {
                     onView.textContent = "This work is currently on view at the Art Insitute of Chicago!"
                 }
+                else {onView.textContent = "This work is NOT currently on view at the Art Insitute of Chicago."};
                 
-
                 //image display 
                 var imageURL = data['config']['iiif_url'];
                 var imageRetrieve = imageURL + "/" + data['data'][i]['image_id'] + "/full/843,/0/default.jpg";
@@ -74,67 +80,39 @@ function getApi(valueSearched) {
             }
 
         });
-
+};
     //fetch data via URL
-    fetch(WikipediaUrl)
-        .then(function (response) {
-            return response.json();
+    //fetch(WikipediaUrl)
+    //   .then(function (response) {
+     //       return response.json();
+       // })
+
+        //.then(function (data) {
+      //      console.log(data);
+
+      //  });
+//}
+
+function displaySearch() {
+        console.log(localStorage.getItem("recentSearch"));
+        var recentSearchBtn = document.createElement('button');
+        recentSearchBtn.classList.add('button', 'is-primary', 'is-rounded', 'mt-2');
+        recentSearchBtn.textContent = localStorage.getItem("recentSearch");
+        recentSearches.appendChild(recentSearchBtn);  
+
+        recentSearchBtn.addEventListener("click", function(){
+            inputValue.value = recentSearchBtn.textContent;
         })
+};
 
-        .then(function (data) {
-            console.log(data);
 
-        });
-}
 
-submitBtn.addEventListener("click", function (event) {
-    // valueSearched = document.getElementById("userSearch").value;
+submitBtn.addEventListener("click", function(){
     getApi();
-    i = 0;
-    // displaySearch();
-}
-);
-submitBtn.addEventList
+    const searchInput = document.getElementById('inputValue');
 
-// //display users choices on screen as list items
-// function displaySearch() {
-//     listOfArtworkSearches.classList.add("card");
-//     var valueSearched = document.getElementById("userSearch").value;
-//     //created an array to hold inputs, add the OR statement because if there isnt anything in the array, returns undefined
-//     searchArray = JSON.parse(localStorage.getItem("searchedArtwork")) || [];
-
-//     // if a searched input is already in the search history, don't add new search to history again
-//     if (!searchArray.includes(valueSearched)) {
-//         searchArray.push(valueSearched);
-//     } else {
-//         searchArray
-//     }
-
-//     //setting the array to local storage to save it
-//     localStorage.setItem("searchedArtwork", JSON.stringify(searchArray));
-//     listOfArtworkSearches.innerHTML = '';
-//     //for each item in the Array (from local storage), creating elements and appending it to an existing parent div from the html page, which was declared at the top of the file page
-//     searchArray.forEach(function (searchName) {
-//         var listItem = document.createElement('a');
-//         listItem.classList.add("list-group-item");
-//         listItem.classList.add("btn");
-
-//         var ulItem = document.createElement('ul');
-//         ulItem.classList.add("list-group");
-//         ulItem.append(listItem);
-
-//         listItem.textContent = searchName;
-//         listItem.value = searchName;
-
-//         listOfArtworkSearches.append(ulItem);
-
-//         listItem.addEventListener("click", function (event) {
-//             console.log("clicked search history")
-//             var artworkpicked = event.target.value;
-//             console.log(artworkpicked);
-//             let valueSearched = artworkpicked;
-//             getAPI(valueSearched);
-//         })
-//     });
-
-// };
+  //  clear input field
+    searchInput.value = '';
+    displaySearch();
+});
+//submitBtn.addEventListener("click", displaySearch());
