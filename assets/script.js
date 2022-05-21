f5f25f78abf6a49c3772661de5c35756dc84db0a
Maia Davis <maia@Maias-Air.lan>
2022-05-21 10:35:56 -0500

    changed image sizing

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@
 var inputValue = document.getElementById('inputValue');
 var submitBtn = document.getElementById('submitBtn');
 
+var searchBar = document.getElementById('searchBar');
+var submitWBtn = document.getElementById('submitWBtn');
+
 //artwork info section variables
 var titleOfPieceEl = document.getElementById('titleOfPiece')
 var thumbnailEl = document.getElementById('thumbnail')
@@ -11,6 +14,7 @@ var carousel = document.getElementById('artworkCarousel');
 
 var recentSearches = document.getElementById("localStorage");
 var containerElement = document.getElementById("currentSearch");
+
 
 function getApi() {
     //search Art Insitute Database
@@ -31,32 +35,33 @@ function getApi() {
             carousel.innerHTML = '';
 
             //loop through data for up to 6 artworks
+
             for (var i = 0; i < 6; i++) {
 
-                //create title element
+            //create title element
                 var titleParagraph = document.createElement('p');
                 console.log(titleParagraph);
 
-                //create artist name element
+            //create artist name element
                 var artistParagraph = document.createElement('p');
 
-                //create element showing whether piece is on view
+            //create element showing whether piece is on view
                 var onView = document.createElement('p');
 
-                //populate elements with relevant data
+            //populate elements with relevant data
                 titleParagraph.textContent = "Title of Piece: " + data['data'][i]['title'];
                 artistParagraph.textContent = "Artist: " + data['data'][i]['artist_titles'][0];
 
                 if (data['data'][i]['is_on_view'] === true) {
                     onView.textContent = "This work is currently on view at the Art Insitute of Chicago!"
                 }
-                else { onView.textContent = "This work is NOT currently on view at the Art Insitute of Chicago." };
-
+                else {onView.textContent = "This work is NOT currently on view at the Art Insitute of Chicago."};
+                
                 //image display 
                 var imageURL = data['config']['iiif_url'];
-                var imageRetrieve = imageURL + "/" + data['data'][i]['image_id'] + "/full/843,/0/default.jpg";
+                var imageRetrieve = imageURL + "/" + data['data'][i]['image_id'] + "/full/600,/0/default.jpg";
                 var image = document.createElement('img');
-                image.src = imageURL + "/" + data['data'][i]['image_id'] + "/full/843,/0/default.jpg";
+                image.src = imageURL + "/" + data['data'][i]['image_id'] + "/full/600,/0/default.jpg";
                 console.log(image.src);
                 console.log(imageRetrieve);
                 console.log(image);
@@ -66,29 +71,28 @@ function getApi() {
                 carousel.appendChild(artistParagraph);
                 carousel.appendChild(onView);
                 carousel.appendChild(image);
-
+                
             }
 
         });
 };
 
 function displaySearch() {
-    console.log(localStorage.getItem("recentSearch"));
-    var recentSearchBtn = document.createElement('button');
-    recentSearchBtn.classList.add('button', 'is-primary', 'is-rounded', 'mt-2');
-    recentSearchBtn.textContent = localStorage.getItem("recentSearch");
-    recentSearches.appendChild(recentSearchBtn);
+        console.log(localStorage.getItem("recentSearch"));
+        var recentSearchBtn = document.createElement('button');
+        recentSearchBtn.classList.add('button', 'is-primary', 'is-rounded', 'mt-2');
+        recentSearchBtn.textContent = localStorage.getItem("recentSearch");
+        recentSearches.appendChild(recentSearchBtn);  
 
-    recentSearchBtn.addEventListener("click", function () {
-        inputValue.value = recentSearchBtn.textContent;
-    })
+        recentSearchBtn.addEventListener("click", function(){
+            inputValue.value = recentSearchBtn.textContent;
+        })
 };
 
-submitBtn.addEventListener("click", function () {
-    getApi();
+submitBtn.addEventListener("click", function(){
+    getApi();const searchInput = document.getElementById('inputValue');
 
-    //  clear input field
-    const searchInput = document.getElementById('inputValue');
+  //  clear input field
     searchInput.value = '';
     displaySearch();
 });
